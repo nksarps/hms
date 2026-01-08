867648c8c738fe8ba80ba5e792f3eb4ea1642eae
Nana Kwaku <nksarps@gmail.com>
2026-01-08 16:29:41 +0000

    feat: add PatientFeedback service and controller creation methods in ServiceFactory

--- src/main/java/com/nks/hms/factory/ServiceFactory.java
+++ src/main/java/com/nks/hms/factory/ServiceFactory.java
@@ -4,15 +4,18 @@ import com.nks.hms.controller.AppointmentController;
 import com.nks.hms.controller.DoctorController;
 import com.nks.hms.controller.MedicalInventoryController;
 import com.nks.hms.controller.PatientController;
+import com.nks.hms.controller.PatientFeedbackController;
 import com.nks.hms.controller.PrescriptionController;
 import com.nks.hms.repository.AppointmentRepository;
 import com.nks.hms.repository.DoctorRepository;
 import com.nks.hms.repository.IAppointmentRepository;
 import com.nks.hms.repository.IDoctorRepository;
 import com.nks.hms.repository.IMedicalInventoryRepository;
+import com.nks.hms.repository.IPatientFeedbackRepository;
 import com.nks.hms.repository.IPatientRepository;
 import com.nks.hms.repository.IPrescriptionRepository;
 import com.nks.hms.repository.MedicalInventoryRepository;
+import com.nks.hms.repository.PatientFeedbackRepository;
 import com.nks.hms.repository.PatientRepository;
 import com.nks.hms.repository.PrescriptionRepository;
 import com.nks.hms.service.AppointmentService;
@@ -20,21 +23,25 @@ import com.nks.hms.service.DoctorService;
 import com.nks.hms.service.IAppointmentService;
 import com.nks.hms.service.IDoctorService;
 import com.nks.hms.service.IMedicalInventoryService;
+import com.nks.hms.service.IPatientFeedbackService;
 import com.nks.hms.service.IPatientService;
 import com.nks.hms.service.IPrescriptionService;
 import com.nks.hms.service.MedicalInventoryService;
+import com.nks.hms.service.PatientFeedbackService;
 import com.nks.hms.service.PatientService;
 import com.nks.hms.service.PrescriptionService;
 import com.nks.hms.validation.AppointmentValidator;
 import com.nks.hms.validation.DoctorValidator;
 import com.nks.hms.validation.IValidator;
 import com.nks.hms.validation.MedicalInventoryValidator;
+import com.nks.hms.validation.PatientFeedbackValidator;
 import com.nks.hms.validation.PatientValidator;
 import com.nks.hms.validation.PrescriptionValidator;
 import com.nks.hms.model.Appointment;
 import com.nks.hms.model.Doctor;
 import com.nks.hms.model.MedicalInventory;
 import com.nks.hms.model.Patient;
+import com.nks.hms.model.PatientFeedback;
 import com.nks.hms.model.Prescription;
 
 /**
@@ -167,6 +174,27 @@ public class ServiceFactory {
         return new MedicalInventoryController(inventoryService);
     }
     
+    /**
+     * Creates a configured patient feedback service with all dependencies.
+     * 
+     * @return Fully configured patient feedback service
+     */
+    public static IPatientFeedbackService createPatientFeedbackService() {
+        IPatientFeedbackRepository repository = createPatientFeedbackRepository();
+        IValidator<PatientFeedback> validator = createPatientFeedbackValidator();
+        return new PatientFeedbackService(repository, validator);
+    }
+    
+    /**
+     * Creates a patient feedback controller with injected service.
+     * 
+     * @param feedbackService The patient feedback service to inject
+     * @return Configured patient feedback controller
+     */
+    public static PatientFeedbackController createPatientFeedbackController(IPatientFeedbackService feedbackService) {
+        return new PatientFeedbackController(feedbackService);
+    }
+    
     // Private factory methods for internal dependencies
     
     private static IPatientRepository createPatientRepository() {
@@ -189,6 +217,10 @@ public class ServiceFactory {
         return new MedicalInventoryRepository();
     }
     
+    private static IPatientFeedbackRepository createPatientFeedbackRepository() {
+        return new PatientFeedbackRepository();
+    }
+    
     private static IValidator<Patient> createPatientValidator() {
         return new PatientValidator();
     }
@@ -207,5 +239,9 @@ public class ServiceFactory {
     
     private static IValidator<MedicalInventory> createMedicalInventoryValidator() {
         return new MedicalInventoryValidator();
+    }
+    
+    private static IValidator<PatientFeedback> createPatientFeedbackValidator() {
+        return new PatientFeedbackValidator();
     }
 }
